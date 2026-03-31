a99940b259756a5093e24268d17bda58c8350633
Umar Hasan <umarhasan@Umars-MacBook-Pro.local>
2026-03-31 15:37:41 +0900

    user app working

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-uslpatterns = [
+urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- users/views.py
+++ users/views.py
@@ -1,9 +1,6 @@
-from urllib import request
-
-from django.shortcuts import render
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.contrib.auth import get_user_model, authenticate
 from .serializers import RegisterSerializer
 
@@ -13,6 +10,6 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response({'message': 'User created'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
