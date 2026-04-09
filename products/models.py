3358ddf8b6cc6cb4a62081c90ed34c1695dfb62f
Umar Hasan <autowebpersonal@gmail.com>
2026-04-09 16:00:12 +0900

    addition of new modelStock

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,9 +11,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Stock(models.Model):
+	product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='stock')
+	quantity = models.PositiveIntegerField(default=0)
+	last_update = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f'{self.product.name} - {self.quantity} units'
