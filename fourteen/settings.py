d24ffa4b31d67882343196175793b3fcc52c8b97
Umar Hasan <autowebpersonal@gmail.com>
2026-04-10 14:27:36 +0900

    made some changes and improvements

--- fourteen/settings.py
+++ fourteen/settings.py
@@ -122,7 +122,7 @@ AUTH_USER_MODEL = 'users.User'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, Stock
 
+class StockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'product', 'quantity', 'last_update']
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -8,7 +13,8 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class ProductSerializer(serializers.ModelSerializer):
-
+    stock = StockSerializer(read_only=True)
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'stock', 'category', 'created_at']
+
